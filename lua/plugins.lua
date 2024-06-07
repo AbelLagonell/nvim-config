d859a1830049098cf3d8e565f76fade08528a84d
Abel-Lagonell <alagonell1730@floridapoly.edu>
2024-06-07 11:40:47 -0400

    More stuff added in the plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,55 +1,55 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     -- LSP manager
-	"williamboman/mason.nvim",
-	"williamboman/mason-lspconfig.nvim",
-	"neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
     "nvim-treesitter/nvim-treesitter",
 
     -- Vscode-like pictograms
-	{
-		"onsails/lspkind.nvim",
-		event = { "VimEnter" },
-	},
-	-- Auto-completion engine
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"lspkind.nvim",
-			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-			"hrsh7th/cmp-buffer", -- buffer auto-completion
-			"hrsh7th/cmp-path", -- path auto-completion
-			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
-		},
-		config = function()
-			require("config.nvim-cmp")
-		end,
-	},
-	-- Code snippet engine
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-	},
-    
+    {
+        "onsails/lspkind.nvim",
+        event = { "VimEnter" },
+    },
+    -- Auto-completion engine
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+            "hrsh7th/cmp-buffer",   -- buffer auto-completion
+            "hrsh7th/cmp-path",     -- path auto-completion
+            "hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+        },
+        config = function()
+            require("config.nvim-cmp")
+        end,
+    },
+    -- Code snippet engine
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+    },
+
     --Colorscheme
     "tanvirtin/monokai.nvim",
     'folke/tokyonight.nvim',
 
 
     --Thing at Bottom
-    { 
+    {
         "nvim-lualine/lualine.nvim",
         dependencies = {
             "nvim-tree/nvim-web-devicons"
@@ -73,7 +73,7 @@ require("lazy").setup({
 
     {
         'akinsho/bufferline.nvim',
-        lazy=false,
+        lazy = false,
         dependencies = {
             'nvim-tree/nvim-web-devicons'
         },
@@ -92,20 +92,20 @@ require("lazy").setup({
         "windwp/nvim-ts-autotag",
         config = function()
             require('nvim-ts-autotag').setup({
-              opts = {
-                -- Defaults
-                enable_close = true, -- Auto close tags
-                enable_rename = true, -- Auto rename pairs of tags
-                enable_close_on_slash = false -- Auto close on trailing </
-              },
-              -- Also override individual filetype configs, these take priority.
-              -- Empty by default, useful if one of the "opts" global settings
-              -- doesn't work well in a specific filetype
-              per_filetype = {
-                ["html"] = {
-                  enable_close = false
+                opts = {
+                    -- Defaults
+                    enable_close = true,          -- Auto close tags
+                    enable_rename = true,         -- Auto rename pairs of tags
+                    enable_close_on_slash = false -- Auto close on trailing </
+                },
+                -- Also override individual filetype configs, these take priority.
+                -- Empty by default, useful if one of the "opts" global settings
+                -- doesn't work well in a specific filetype
+                per_filetype = {
+                    ["html"] = {
+                        enable_close = false
+                    }
                 }
-              }
             })
         end
     },
@@ -119,8 +119,8 @@ require("lazy").setup({
     },
 
     {
-        'akinsho/toggleterm.nvim', 
-        version = "*", 
+        'akinsho/toggleterm.nvim',
+        version = "*",
         config = true
     },
 
@@ -130,7 +130,8 @@ require("lazy").setup({
     },
 
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
@@ -148,8 +149,33 @@ require("lazy").setup({
         "stevearc/conform.nvim",
         event = 'BufWritePre',
         config = function()
-            require ("config.conform")
+            require("config.conform")
         end
     },
+
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "User Filepost",
+        config = function(_, opts)
+            dofile(vim.g.base46_cache .. "git")
+            require("gitsigns").setup(opts)
+        end
+    },
+
+    {
+        'norcalli/nvim-colorizer.lua',
+        config = function()
+            require('colorizer').setup()
+        end
+    },
+
+    {
+        'nvim-tree/nvim-web-devicons',
+        config = function()
+            require('config.web-devicons')
+        end
+    },
+
+    { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
 
 })
